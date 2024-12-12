e58417c2133792c6305fb245fc1e61544433d932
jesse <jwang6174@gmail.com>
2024-12-12 05:37:31 -0500

    Add path utility module back

---
+++ pathutil.py
@@ -0,0 +1,5 @@
+import os
+
+DATA_PATH = os.path.join('/', 'home', 'jesse', 'physionet.org', 'files', 'scg-rhc-wearable-database', '1.0.0')
+
+PROCESSED_DATA_PATH = os.path.join(DATA_PATH, 'processed_data')
