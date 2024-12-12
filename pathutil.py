f66a8908fb4febfedcc2f0c003a6a9c683241a02
jesse <jwang6174@gmail.com>
2024-12-12 05:23:16 -0500

    Remove path utility module

--- pathutil.py
+++
@@ -1,5 +0,0 @@
-import os
-
-DATA_PATH = os.path.join('/', 'home', 'jesse', 'physionet.org', 'files', 'scg-rhc-wearable-database', '1.0.0')
-
-PROCESSED_DATA_PATH = os.path.join(DATA_PATH, 'processed_data')
