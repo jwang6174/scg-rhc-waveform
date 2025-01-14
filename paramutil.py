48243fcd5990d89e32b6c41e10d39b6d4070a5f9
jesse <jwang6174@gmail.com>
2025-01-14 15:21:23 -0500

    Make separate directories for random and top plots

--- paramutil.py
+++ paramutil.py
@@ -14,13 +14,18 @@ class Params:
     self.train_path = os.path.join(self.dir_path, self.data['train_path'])
     self.valid_path = os.path.join(self.dir_path, self.data['valid_path'])
     self.test_path = os.path.join(self.dir_path, self.data['test_path'])
-    self.checkpoint_path = os.path.join(self.dir_path, self.data['checkpoint_path'])
+    self.checkpoint_dir_path = os.path.join(self.dir_path, self.data['checkpoint_dir_path'])
     self.losses_fig_path = os.path.join(self.dir_path, self.data['losses_fig_path'])
     self.comparisons_path = os.path.join(self.dir_path, self.data['comparisons_path'])
-    self.pred_RHC_path = os.path.join(self.dir_path, self.data['pred_RHC_path'])
+    self.pred_top_dir_path = os.path.join(self.dir_path, self.data['pred_top_dir_path'])
+    self.pred_rand_dir_path = os.path.join(self.dir_path, self.data['pred_rand_dir_path'])
     self.alpha = self.data['alpha']
     self.beta1 = self.data['beta1']
     self.beta2 = self.data['beta2']
+    self.n_critic = self.data['n_critic']
+    self.lambda_gp = self.data['lambda_gp']
+    self.lambda_aux = self.data['lambda_aux']
+    self.total_epochs = self.data['total_epochs']
 
   def init_json(self, path):
     with open(path, 'r') as f:
