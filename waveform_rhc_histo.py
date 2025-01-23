f108e25d6036aeda9e88f5c7bc58aa4743d845d3
jesse <jwang6174@gmail.com>
2025-01-23 10:29:11 -0500

    Create histogram of RHC values

---
+++ waveform_rhc_histo.py
@@ -0,0 +1,16 @@
+import matplotlib.pyplot as plt
+import os
+import wfdb
+from pathutil import PROCESSED_DATA_PATH
+from recordutil import get_record_names
+
+rhc_vals = []
+for name in get_record_names():
+  record = wfdb.rdrecord(os.path.join(PROCESSED_DATA_PATH, name))
+  if 'RHC_pressure' in record.sig_name:
+    rhc_index = record.sig_name.index('RHC_pressure')
+    rhc_channel = record.p_signal[:, rhc_index]
+    rhc_vals.extend(rhc_channel)
+
+plt.hist(rhc_vals)
+plt.savefig('rhc_histo.png')
