a2d1cee69afb41d9f7193a1e6f5732b992aa1708
jesse <jwang6174@gmail.com>
2025-01-23 08:41:43 -0500

    Remove extra spacing

--- waveform_test.py
+++ waveform_test.py
@@ -106,16 +106,14 @@ def run(params, checkpoint):
   save_random_pred_plots(params.pred_rand_dir_path, generator, train_loader, num_plots=5)
   save_random_pred_plots(params.pred_rand_dir_path, generator, test_loader, num_plots=5)
   
-  comparisons = get_waveform_comparisons(generator, test_loader)
-  comparisons.sort(key=lambda x: x['dtw'])
-
-  comparisons_df = pd.DataFrame(comparisons)
-  comparisons_df.to_csv(params.comparisons_path, index=False)
-
-  save_top_pred_plots(params, generator, comparisons, num_plots=100)
+  # comparisons = get_waveform_comparisons(generator, test_loader)
+  # comparisons.sort(key=lambda x: x['dtw'])
+  # comparisons_df = pd.DataFrame(comparisons)
+  # comparisons_df.to_csv(params.comparisons_path, index=False)
+  # save_top_pred_plots(params, generator, comparisons, num_plots=100)
 
 
 if __name__ == '__main__':
-  params = Params('03_waveform/params.json')
-  run(params, '162.checkpoint')
+  params = Params('04_waveform/params.json')
+  run(params, '000.checkpoint')
 
