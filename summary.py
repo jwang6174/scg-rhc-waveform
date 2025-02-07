acd9452120a724a93e24efea874b1af101df6a14
jesse <jwang6174@gmail.com>
2025-02-07 17:09:46 -0500

    Revamp summary module

--- summary.py
+++ summary.py
@@ -4,112 +4,96 @@ import os
 import pandas as pd
 import wfdb
 from pathutil import PROCESSED_DATA_PATH
-from recordutil import get_record_names, get_segments, get_chamber_intervals, SAMPLE_FREQ
-    
-
-record_names = get_record_names()
-
-jsons = []
-for record_name in record_names:
-  path = os.path.join(PROCESSED_DATA_PATH, f'{record_name}.json')
-  with open(path, 'r') as f:
-    data = json.load(f)
-    data['filename'] = record_name
-    jsons.append(data)
-df = pd.DataFrame(jsons)
-
-records = []
-for record_name in record_names:
-  record = wfdb.rdrecord(os.path.join(PROCESSED_DATA_PATH, record_name))
-  records.append(record)
+from recordutil import get_record_names, get_chamber_intervals, SAMPLE_FREQ
+from scipy.stats import ttest_ind
 
 
-def summarize(df):
+def get_modified_maclab_meas(original):
+  modified = {}
+  for key, val in original.items():
+    key = key.strip()
+    if isinstance(val, str):
+      modified[key] = np.nan
+    else:
+      modified[key] = val
+  return modified
 
-  print(f"Record count: {len(df)}")
 
-  print("Age (yr)")
-  print(f"  Min {df['age'].min():.2f}")
-  print(f"  Max {df['age'].max():.2f}")
-  print(f"  Avg {df['age'].mean():.2f} ± {df['age'].std():.2f}")
+def get_df():
+  df = {}
+  for record_name in get_record_names():
+    path = os.path.join(PROCESSED_DATA_PATH, f'{record_name}.json')
+    with open(path, 'r') as f:
+      data = json.load(f)
+      data['record_name'] = record_name
+      data['sbp'] = np.nan if data['sbp'] == -1 else data['sbp']
+      data['dbp'] = np.nan if data['dbp'] == -1 else data['dbp']
+      data.update(get_modified_maclab_meas(data['maclabMeas']))
+      df[record_name] = data
+  return df
 
-  print("Height (cm)")
-  print(f"  Min {df['height'].min():.2f}")
-  print(f"  Max {df['height'].max():.2f}")
-  print(f"  Avg {df['height'].mean():.2f} ± {df['height'].std():.2f}")
 
-  print(f"Weight (kg)")
-  print(f"  Min {df['weight'].min():.2f}")
-  print(f"  Max {df['weight'].max():.2f}")
-  print(f"  Avg {df['weight'].mean():.2f} ± {df['weight'].std():.2f}")
+def get_signal_names(records):
+  names = set()
+  for record in records:
+    names.update(record.sig_name)
+  return names
 
-  print("SBP (mmHg)")
-  print(f"  Min {df['sbp'].min():.2f}")
-  print(f"  Max {df['sbp'].max():.2f}")
-  print(f"  Avg {df['sbp'].mean():.2f} ± {df['sbp'].std():.2f}")
 
-  print("DBP (mmHg)")
-  print(f"  Min {df['dbp'].min():.2f}")
-  print(f"  Max {df['dbp'].max():.2f}")
-  print(f"  Avg {df['dbp'].mean():.2f} ± {df['dbp'].std():.2f}")
+def add_signal_presence(df):
+  record_names = df.keys()
+  records = [wfdb.rdrecord(os.path.join(PROCESSED_DATA_PATH, x)) for x in record_names]
+  signal_names = get_signal_names(records)
+  for record_name, record in zip(record_names, records):
+    for signal_name in signal_names:
+      df[record_name][signal_name] = signal_name in record.sig_name
 
-  print("Clinical status")
-  print(f"  Comp {df['CDecomp'].value_counts().get(0)}")
-  print(f"  Decomp {df['CDecomp'].value_counts().get(1)}")
 
-  print(f"Physiologic status")
-  print(f"  Comp {df['PDecomp'].value_counts().get(0)}")
-  print(f"  Decomp {df['PDecomp'].value_counts().get(1)}")
+def add_chamber_durations(df):
+  for record_name in df.keys():
+    chamber_times = {
+      'RA': 0,
+      'RV': 0,
+      'PA': 0,
+      'PCW': 0
+    }
+    for chamber in chamber_times.keys():
+      for start, end in get_chamber_intervals(record_name, chamber):
+        time = (end - start) / SAMPLE_FREQ
+        chamber_times[chamber] += time
+    for chamber, time in chamber_times.items():
+      df[record_name][chamber] = time
 
-  print("NYHAC")
-  print(f"  1 {df['NYHAC'].value_counts().get(1)}")
-  print(f"  2 {df['NYHAC'].value_counts().get(2)}")
-  print(f"  3 {df['NYHAC'].value_counts().get(3)}")
-  print(f"  4 {df['NYHAC'].value_counts().get(4)}")
-  print(f"  Avg {df['NYHAC'].mean():.2f} ± {df['NYHAC'].std():.2f}")
 
-  print("Physiologic challenge")
-  print(f"  Y {df['IsChallenge'].value_counts().get(True)}")
-  print(f"  N {df['IsChallenge'].value_counts().get(False)}")
+def summarize_numerical(df, var, gender_stratified):
+  print(var)
+  print(f"  Min {df[var].min():.2f}")
+  print(f"  Max {df[var].max():.2f}")
+  print(f"  Avg {df[var].mean():.2f} ± {df[var].std():.2f}")
+  if not gender_stratified:
+    group1 = df[df['gender'] == 'Male'][var]
+    group2 = df[df['gender'] == 'Female'][var]
+    _, p_value = ttest_ind(group1, group2, nan_policy='omit')
+    print(f"  Sig {p_value:.2f}")
 
-  print("Outpatient")
-  print(f"  Y {df['outpatient'].value_counts().get(True)}")
-  print(f"  N {df['outpatient'].value_counts().get(False)}")
 
-  print("Heart failure")
-  print(f"  Y {df['heart failure'].value_counts().get(True)}")
-  print(f"  N {df['heart failure'].value_counts().get(False)}")
+def summarize_boolean(df, var):
+  print(var)
+  print(f"  Y {df[var].value_counts().get(True)}")
+  print(f"  N {df[var].value_counts().get(False)}")
 
-  print("Missing Maclab RHC")
-  print(f"  Y {df['Missing_MaclabRHC'].value_counts().get(True)}")
-  print(f"  N {df['Missing_MaclabRHC'].value_counts().get(False)}")
 
-  print("Fine alignment")
-  print(f"  Y {df['fine_alignment'].value_counts().get(True)}")
-  print(f"  N {df['fine_alignment'].value_counts().get(False)}")
-
-  print("Diagnosed with HF")
-  print(f"  Y {df['heart failure'].value_counts().get(True)}")
-  print(f"  N {df['heart failure'].value_counts().get(False)}")
-
-  print("Outpatient")
-  print(f"  Y {df['outpatient'].value_counts().get(True)}")
-  print(f"  N {df['outpatient'].value_counts().get(False)}")
-
-  meas_dicts = []
-  print('Missing maclabMeas')
-  for _, row in df.iterrows():
-    mod_dict = {}
-    for key, val in row['maclabMeas'].items():
-      if isinstance(val, str):
-        print(f"  {row['filename']} {key} {val}")
-        mod_dict[key.strip()] = np.nan
-      else:
-        mod_dict[key.strip()] = val
-    meas_dicts.append(mod_dict)
-  meas_df = pd.DataFrame.from_dict(meas_dicts)
-
-  meas_vals = [
+def summarize(df, gender_stratified):
+  
+  numerical_vars = [
+    'age',
+    'bmi',
+    'sbp',
+    'dbp',
+    'RA',
+    'RV',
+    'PA',
+    'PCW',
     'RAA Wave',
     'RAV Wave',
     'RAM',
@@ -132,42 +116,40 @@ def summarize(df):
     'Avg. COmL/min',
     'SVmL/beat',
   ]
+  
+  boolean_vars = [
+    'Missing_MaclabRHC',
+    'fine_alignment',
+    'outpatient',
+  ]
+  
+  for var in numerical_vars:
+    summarize_numerical(df, var, gender_stratified)
+  
+  for var in boolean_vars:
+    summarize_boolean(df, var)
+  
+  print("NYHAC")
+  print(f"  1 {df['NYHAC'].value_counts().get(1)}")
+  print(f"  2 {df['NYHAC'].value_counts().get(2)}")
+  print(f"  3 {df['NYHAC'].value_counts().get(3)}")
+  print(f"  4 {df['NYHAC'].value_counts().get(4)}")
 
-  for val in meas_vals:
-    print(val)
-    print(f"  Min {meas_df[val].min():.2f}")
-    print(f"  Max {meas_df[val].max():.2f}")
-    print(f"  Avg {meas_df[val].mean():.2f} ± {meas_df[val].std():.2f}")
 
-  chamber_times = {
-    'RA': [],
-    'RV': [],
-    'PA': [],
-    'PCW': [],
-  }
+if __name__ == '__main__':
+  df = get_df()
+  add_signal_presence(df)
+  add_chamber_durations(df)
+  df = [val for val in df.values()]
+  df = pd.DataFrame.from_dict(df)
+  df['bmi'] = df['weight'] / ((df['height'] / 100) ** 2) 
+ 
+  print('\n----- All -----')
+  summarize(df, gender_stratified=False)
 
-  for chamber in chamber_times:
-    for _, row in df.iterrows():
-      time = 0
-      for start_index, end_index in get_chamber_intervals(row['filename'], chamber):
-        start_sec = start_index / SAMPLE_FREQ
-        end_sec = end_index / SAMPLE_FREQ
-        time += (end_sec - start_sec)
-      chamber_times[chamber].append(time)
+  print('\n----- Male -----')
+  summarize(df[df['gender'] == 'Male'], gender_stratified=True)
 
-  for chamber, time in chamber_times.items():
-    total = np.sum(time)
-    avg = np.mean(time)
-    std = np.std(time)
-    print(f"{chamber} (mmHg)")
-    print(f"  Avg {avg:.2f} ± {std:.2f}")
-    print(f"  Sum {total}")
+  print('\n----- Female -----')
+  summarize(df[df['gender'] == 'Female'], gender_stratified=True)
 
-print('\n----- All -----\n')
-summarize(df)
-
-print('\n----- Male -----\n')
-summarize(df[df['gender'] == 'Male'])
-
-print('\n----- Female -----\n')
-summarize(df[df['gender'] == 'Female'])
