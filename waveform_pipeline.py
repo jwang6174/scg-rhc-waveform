bb522f00ee67789eff38d1e5b46188af1aea22ce
jesse <jwang6174@gmail.com>
2025-02-08 08:56:21 -0500

    Ignore exception if loader pickle file already exists

--- waveform_pipeline.py
+++ waveform_pipeline.py
@@ -8,7 +8,10 @@ from waveform_epochs import run as waveform_epochs
 
 
 def run(params):
-  recordutil(params)
+  try:
+    recordutil(params)
+  except Exception as e:
+    print(e)
   waveform_train(params)
   waveform_test(params, 'valid', 'all')
   waveform_epochs(params)
