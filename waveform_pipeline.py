90bde5376fe2b92a02934bd1635dfc86e98e7869
jesse <jwang6174@gmail.com>
2025-02-09 23:46:40 -0500

    Add option to pipeline to process all active projects

--- waveform_pipeline.py
+++ waveform_pipeline.py
@@ -8,19 +8,33 @@ from waveform_checkpoint import run as waveform_checkpoint
 
 
 def run(params):
+  
   try:
     recordutil(params)
   except Exception as e:
     print(e)
+  
   waveform_train(params)
+  
   try:
     waveform_test(params, 'valid', 'all')
   except Exception as e:
     print(e)
+  
   waveform_checkpoint(params)
+  
+  with open(os.path.join(params.dir_path, 'checkpoint_best.txt'), 'r') as f:
+    best_checkpoint = f.read().splitlines()[0].split()[1]
+    waveform_test(params, 'test', best_checkpoint)
 
 
 if __name__ == '__main__':
   dir_name = sys.argv[1]
-  params = Params(os.path.join(dir_name, 'params.json'))
-  run(params)
+  if dir_name == 'all':
+    for i in range(6, 34):
+      dir_name = f'waveform_{i:02d}'
+      params = Params(os.path.join(dir_name, 'params.json'))
+      run(params)
+  else:
+    params = Params(os.path.join(dir_name, 'params.json'))
+    run(params)
